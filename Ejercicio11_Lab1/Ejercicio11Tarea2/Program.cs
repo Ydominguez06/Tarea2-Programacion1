a3961018839d48d0df031761bfdb7607e4b25aa6
Diego Emiliano Nolasco Mejía <diego.nolasco.e24@gmail.com>
2025-02-14 20:54:56 -0600

    Add files via upload

---
+++ Ejercicio11_Lab1/Ejercicio11Tarea2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ejercicio11Tarea2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double promediototal, nota1, nota2, nota3;
+
+            Console.WriteLine("Ingrese sus notas:");
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+            nota3 = double.Parse(Console.ReadLine());
+
+            promediototal = (nota1 + nota2 + nota3) / 3;
+
+            if (promediototal < 70)
+            {
+                Console.WriteLine("Reprueba");
+            }
+            else if (promediototal <= 80)
+            {
+                Console.WriteLine("Buena nota");
+            }
+            else if (promediototal <= 90)
+            {
+                Console.WriteLine("Muy buena nota");
+            }
+            else if (promediototal <= 100)
+            {
+                Console.WriteLine("Sobresaliente");
+            }
+            else
+            {
+                Console.WriteLine("No se pudo calcular su promedio");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio7_Lab1/Ejercicio7Tarea2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7Tarea2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string letra;
+            Console.WriteLine("Ingrese su caractér: ");
+            letra=Console.ReadLine();
+            if (letra == "a")
+            {
+                Console.WriteLine($"La letra {letra} es una vocal");
+            }
+            else if (letra == "e")
+            {
+                Console.WriteLine($"La letra {letra} es una vocal");
+            }
+            else if (letra == "i")
+            {
+                Console.WriteLine($"La letra {letra} es una vocal");
+            }
+            else if (letra == "o")
+            {
+                Console.WriteLine($"La letra {letra} es una vocal");
+            }
+            else if (letra == "u")
+            {
+                Console.WriteLine($"La letra {letra} es una vocal");
+            }
+            else
+            {
+                Console.WriteLine($"La letra {letra} no es una vocal");
+            }
+            Console.ReadLine();
+        }
+    }
+}
